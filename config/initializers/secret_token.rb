28a2d133745bd214fabfb440ccd5674fa25bc74b
rachel <rchl.twnsnd@gmail.com>
2014-01-26 22:15:34 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweetrage::Application.config.secret_key_base = 'bb41da8285b2e12a26d191d55757fa29dfd3353a9e8082c169f89d2bbbb62a1b14022a183f464ac4ce55e87d5bc12773bca469260178a8575f322e59e9d803d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tweetrage::Application.config.session_store :cookie_store, key: '_tweetrage_session'
